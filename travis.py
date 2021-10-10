3feeef52a0a2c11a737669294f11a59b1758adf1
Rahul Bajaj <rahulrb0509@gmail.com>
2021-10-10 13:05:10 -0400

    TravisCI repositories having issue tracker on github

--- travis.py
+++ travis.py
@@ -1,8 +1,8 @@
+import csv
 import os
 import requests
 import time
 from pprint import pprint
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,6 +12,15 @@ TRAVIS_ISSUES_PARAMS = os.getenv('TRAVIS_ISSUES_PARAMS')
 TRAVIS_FILE_PARAMS = os.getenv('TRAVIS_FILE_PARAMS')
 GITHUB_REPO_URL = os.getenv('GITHUB_REPO_URL')
 PROJECT_PATH = os.getenv('PROJECT_PATH')
+OUTPUT_FILE_FIELD_NAMES = [
+    "id",
+    "repo_name",
+    "forks",
+    "open_issues",
+    "size",
+    "watchers",
+    "stargazers_count",
+]
 
 OUTPUT_FILE_PATH = f"{PROJECT_PATH}/repositories.csv"
 
@@ -45,6 +54,12 @@ def create_csv_file_if_necessary(file_path, headers):
             writer = csv.DictWriter(csv_file, headers)
             writer.writeheader()
 
+def write_lines_to_existing_csv(output_file_path, headers, dicts_to_write):
+    with open(output_file_path, 'a') as csv_file:
+        writer = csv.DictWriter(csv_file, headers)
+        for line in dicts_to_write:
+            writer.writerow(line)
+
 def main():
   repository_data = list()
   if GITHUB_ACCESS_TOKEN is None:
@@ -53,8 +68,26 @@ def main():
   repo_names = get_repository_name(travis_issues)
   for repo in repo_names:
     repository_data.append(send_request(GITHUB_REPO_URL+repo, GITHUB_ACCESS_TOKEN))
-  pprint(repository_data)
   create_csv_file_if_necessary(OUTPUT_FILE_PATH, OUTPUT_FILE_FIELD_NAMES)
+  count = 0
+
+  try:
+    lines_to_write = list()
+    for data in repository_data:
+      lines_to_write.append({
+        "id": data["id"],
+        "repo_name": repo_names[count],
+        "forks": data["forks"],
+        "open_issues" : data["open_issues"],
+        "size": data["size"],
+        "watchers": data["watchers"],
+        "stargazers_count": data["stargazers_count"]
+      })
+      count += 1
+    write_lines_to_existing_csv(OUTPUT_FILE_PATH, OUTPUT_FILE_FIELD_NAMES, lines_to_write)
+  except Exception as e:
+      print(f"[ERROR] check the format of repository data")
+  print("DONE")
 
 if __name__ == "__main__":
     main()
